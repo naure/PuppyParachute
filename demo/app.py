a22c208a20241d075f9b8e8618c77ad0e9ad019e
Aure N <aurelien.nicolas.an@gmail.com>
2014-10-13 22:54:03 +0200

    Include the annotations and RunStore after tracing the demo test

--- demo/app.py
+++ demo/app.py
@@ -1,7 +1,11 @@
 
+
+#? what=that -> Done
 def do_things(what):
     print('Doing {}'.format(what))
+    return 'Done'
 
+#?  -> Everything is alright
 def main():
     do_things('this')
     do_things('that')
